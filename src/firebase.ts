8fcc9d586d1e41089fc68344fd641dec971c5d94
jason4128 <98997560+jason4128@users.noreply.github.com>
2026-06-24 14:58:53 +0800

    1111

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInAnonymously } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  projectId: "japantravel-8e369",
-  appId: "1:473684537567:web:83fa8492b9f5033a78b96a",
-  apiKey: "AIzaSyCHAQijbCdeWpxTsJyqzBhqucyWWyQPJVw",
-  authDomain: "japantravel-8e369.firebaseapp.com",
-  storageBucket: "japantravel-8e369.firebasestorage.app",
-  messagingSenderId: "473684537567",
-  measurementId: ""
-};
+import firebaseConfig from "../firebase-applet-config.json";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 
 export const signIn = async () => {
   try {
